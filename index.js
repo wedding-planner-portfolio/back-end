b9789f806e19c71539750d9a6bc7e4fcacc519e4
Ronny Alvarado <rsalvarado777@gmail.com>
2019-07-10 21:10:54 -0500

    Created index.js file boiler plate
    Co-authored-by: Ronny Alvarado <Rsalvarado777@gmail.com>
    Co-authored-by: Edgar Flores <eflores.ef@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//testing co-author-commits
+require("dotenv").config();
+
+const server = require("./api/server.js");
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
