258f5982003e0dd5dbab878f48c2d13fd8b1f52e
Ronny Alvarado <rsalvarado777@gmail.com>
2019-07-13 12:58:26 -0500

    Updated server.js to use authRouter
    Co-authored-by: Ronny Alvarado <Rsalvarado777@gmail.com>
    Co-authored-by: Edgar Flores <eflores.ef@gmail.com>

--- api/server.js
+++ api/server.js
@@ -3,9 +3,13 @@ const helmet = require("helmet");
 const cors = require("cors");
 const server = express();
 
+const authRouter = require("../auth/authRouter");
+
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+
+server.use("/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "API running." });
